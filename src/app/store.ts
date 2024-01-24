d9d1310489a9f8d5cd65af353d1d90614df21183
Danh <handoiditu1508@gmail.com>
2024-01-25 01:05:37 +0700

    config redux logger for dev env

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 
+// development environment only
+// const reduxLogger = require("redux-logger");
+// const logger = reduxLogger.createLogger();
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 export type AppDispatch = typeof store.dispatch;
