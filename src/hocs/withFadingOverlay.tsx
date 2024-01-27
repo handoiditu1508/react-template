4b0e16427da36d361fe4c3550a3e226f56ef5ff4
Danh <handoiditu1508@gmail.com>
2024-01-27 15:27:14 +0700

    Create withFadingOverlay.tsx

---
+++ src/hocs/withFadingOverlay.tsx
@@ -0,0 +1,11 @@
+import { Fade, Portal } from "@mui/material";
+
+const withFadingOverlay = <P extends JSX.IntrinsicAttributes>(WrappedComponent: React.ComponentType<P>) => {
+  return ({ isShow, ...props }: P & { isShow?: boolean; }) => <Portal>
+    <Fade in={isShow}>
+      <WrappedComponent {...(props as P)} />
+    </Fade>
+  </Portal>;
+};
+
+export default withFadingOverlay;
