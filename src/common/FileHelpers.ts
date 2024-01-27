8823900c19b4538341c8037534117eb6f40cd9ab
Danh <handoiditu1508@gmail.com>
2024-01-27 11:42:13 +0700

    Create FileHelpers.ts

---
+++ src/common/FileHelpers.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+export const loadFileFromUrl = async (url: string, name?: string): Promise<File> => {
+  const response = await axios.get<Blob>(url, { responseType: "blob" });
+  const file = new File([response.data], name || uuidv4(), { type: response.headers["content-type"] });
+  return file;
+};
