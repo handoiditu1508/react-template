d73a6f27f27a8f3976ecda57ffe869dfee19a318
Danh <handoiditu1508@gmail.com>
2024-01-27 11:44:09 +0700

    Create Formats.ts

---
+++ src/common/Formats.ts
@@ -0,0 +1,9 @@
+export const percentFormat = (value: number) => `${value}%`;
+export const reversedPercentFormat = (value: number) => `${100 - value}%`;
+export const camelToKebabCase = (value: string) => value.replace(/[A-Z]/g, c => `-${c.toLowerCase()}`);
+export const camelToTitleCase = (value: string) => value.charAt(0).toUpperCase() + value.replace(/[A-Z]/g, c => ` ${c}`).slice(1);
+export const kebabToTitleCase = (value: string) => value.replace(/(^\w|-\w)/g, s => s.replace(/-/, " ").toUpperCase());
+export const kebabToPascalCase = (value: string) => value.replace(/(^\w|-\w)/g, kebabReplacer);
+export const kebabToCamelCase = (value: string) => value.replace(/-\w/g, kebabReplacer);
+
+const kebabReplacer = (substring: string) => substring.replace(/-/, "").toUpperCase();
