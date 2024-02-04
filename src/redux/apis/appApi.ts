aa43027068ee6f451de9cbe445faca00d86a4e64
Danh <handoiditu1508@gmail.com>
2024-02-04 11:05:48 +0700

    handle cache invalidation

--- src/redux/apis/appApi.ts
+++ src/redux/apis/appApi.ts
@@ -14,9 +14,16 @@ const appApi = createApi({
       query: (id) => ({
         url: `posts/${id}`,
       }),
+      providesTags: (_result, _error, id) => [{ type: "Post", id }],
     }),
     getPosts: builder.query<Post[], void>({
       query: () => "posts",
+      providesTags: (result) => result
+        ? [
+          ...result.map(({ id }) => ({ type: "Post", id } as const)),
+          { type: "Post", id: "LIST" },
+        ]
+        : [{ type: "Post", id: "LIST" }],
     }),
     addPost: builder.mutation<Post, Partial<Post>>({
       query: (body) => ({
@@ -24,6 +31,7 @@ const appApi = createApi({
         method: "POST",
         body,
       }),
+      invalidatesTags: [{ type: "Post", id: "LIST" }],
     }),
     updatePost: builder.mutation<Post, Partial<Post> & Pick<Post, "id">>({
       query: (body) => ({
@@ -31,12 +39,14 @@ const appApi = createApi({
         method: "PUT",
         body,
       }),
+      invalidatesTags: (_result, _error, body) => [{ type: "Post", id: body.id }],
     }),
     deletePost: builder.mutation<{ success: boolean; id: number; }, number>({
       query: (id) => ({
         url: `posts/${id}`,
         method: "DELETE",
       }),
+      invalidatesTags: (_result, _error, id) => [{ type: "Post", id }],
     }),
   })),
 });
