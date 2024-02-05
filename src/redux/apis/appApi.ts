444c08990fb7c720f2d2e4dc24625b5c751c4c4f
Danh <handoiditu1508@gmail.com>
2024-02-06 01:29:35 +0700

    split appApi

--- src/redux/apis/appApi.ts
+++ src/redux/apis/appApi.ts
@@ -1,85 +1,12 @@
-import SignInResponse from "@/models/apis/SignInResponse";
-import { QueryReturnValue } from "@reduxjs/toolkit/dist/query/baseQueryTypes";
-import { FetchBaseQueryError, createApi } from "@reduxjs/toolkit/query/react";
-import { clearAuthState, setAuthState } from "../slices/authenticationSlice";
+import { createApi } from "@reduxjs/toolkit/query/react";
 import axiosBaseQuery from "./axiosBaseQuery";
 import reauthBaseQueryWrapper from "./reauthBaseQueryWrapper";
-
-export type Post = {
-  id: number;
-  name: string;
-}
 
 const appApi = createApi({
   reducerPath: "appApi",
   baseQuery: reauthBaseQueryWrapper(axiosBaseQuery({ baseUrl: "/api/" })),
   tagTypes: ["Post"],
-  endpoints: ((builder) => ({
-    getPost: builder.query<Post, number>({
-      query: (id) => ({
-        url: `posts/${id}`,
-      }),
-      providesTags: (_result, _error, id) => [{ type: "Post", id }],
-    }),
-    getPosts: builder.query<Post[], void>({
-      query: () => "posts",
-      providesTags: (result) => result
-        ? [
-          ...result.map(({ id }) => ({ type: "Post", id } as const)),
-          { type: "Post", id: "LIST" },
-        ]
-        : [{ type: "Post", id: "LIST" }],
-    }),
-    addPost: builder.mutation<Post, Partial<Post>>({
-      query: (body) => ({
-        url: "posts",
-        method: "POST",
-        body,
-      }),
-      invalidatesTags: [{ type: "Post", id: "LIST" }],
-    }),
-    updatePost: builder.mutation<Post, Partial<Post> & Pick<Post, "id">>({
-      query: (body) => ({
-        url: `posts/${body.id}`,
-        method: "PUT",
-        body,
-      }),
-      invalidatesTags: (_result, _error, body) => [{ type: "Post", id: body.id }],
-    }),
-    deletePost: builder.mutation<{ success: boolean; id: number; }, number>({
-      query: (id) => ({
-        url: `posts/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: (_result, _error, id) => [{ type: "Post", id }],
-    }),
-    refreshToken: builder.mutation<SignInResponse, void>({
-      queryFn: async (arg, api, _extraOptions, baseQuery) => {
-        const res = await baseQuery({
-          url: "refreshToken",
-          method: "POST",
-          body: arg,
-        }) as QueryReturnValue<SignInResponse, FetchBaseQueryError>;
-
-        if (res.data) {
-          api.dispatch(setAuthState(res.data));
-        } else if (res.error.status === 401) {
-          api.dispatch(clearAuthState());
-        }
-
-        return res as QueryReturnValue<SignInResponse, any>;
-      },
-    }),
-  })),
+  endpoints: (_builder) => ({}),
 });
 
 export default appApi;
-
-export const {
-  useGetPostQuery,
-  useGetPostsQuery,
-  useAddPostMutation,
-  useUpdatePostMutation,
-  useDeletePostMutation,
-  useRefreshTokenMutation,
-} = appApi;
