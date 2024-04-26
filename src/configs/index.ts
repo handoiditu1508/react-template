2ba8c0550f576d383e5fe9122267c05248ca4c8f
Danh <handoiditu1508@gmail.com>
2024-04-27 01:53:40 +0700

    fix build error after migrate to Vite

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,7 @@
 import defaultConfig from "./config";
 import type ConfigType from "./ConfigType";
 
-const environmentConfig: Partial<ConfigType> = (await import(`./${import.meta.env.MODE}.config.ts`)).default;
+const environmentConfig: ConfigType | Partial<ConfigType> = require(`./${import.meta.env.MODE}.config`).default;
 
 const CONFIG: ConfigType = {
   ...defaultConfig,
