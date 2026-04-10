4c49a0b7612e9ac6155fa60cd2f240df47b19e5e
Danh <handoiditu1508@gmail.com>
2026-04-11 00:16:30 +0700

    add route-based breadcrumbs

---
+++ src/components/RouteBasedBreadcrumbs.tsx
@@ -0,0 +1,64 @@
+import { CrumbData, RouteHandleObject } from "@/routes/models";
+import Breadcrumbs, { BreadcrumbsProps } from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import { UIMatch, useMatches } from "react-router-dom";
+import CustomLink from "./CustomLink";
+
+function RouteBasedBreadcrumbs(props: Omit<BreadcrumbsProps, "children">) {
+  const matches = useMatches() as UIMatch<any, RouteHandleObject>[];
+  const currentMatch = matches[matches.length - 1];
+
+  if (currentMatch.handle && currentMatch.handle.hideBreadcrumbs) {
+    return null;
+  }
+
+  const crumbs: CrumbData[] = matches
+    .filter((match) => match.handle && match.handle.crumb)
+    .map((match) => match.handle.crumb!(match.loaderData));
+
+  return (
+    <Breadcrumbs aria-label="breadcrumb" maxItems={5} {...props}>
+      {crumbs.map((crumb, index, array) => {
+        const isLast = index === array.length - 1;
+
+        return (crumb.to === undefined)
+          ? (
+            <Typography
+              key={index}
+              color={isLast ? "textPrimary" : "inherit"}
+              display="flex"
+              alignItems="center"
+              gap={0.5}
+              sx={{
+                "*": {
+                  fontSize: "inherit",
+                },
+              }}>
+              {crumb.icon}
+              {crumb.label}
+            </Typography>
+          )
+          : (
+            <CustomLink
+              key={index}
+              to={crumb.to}
+              underline="hover"
+              color={isLast ? "textPrimary" : "inherit"}
+              display="flex"
+              alignItems="center"
+              gap={0.5}
+              sx={{
+                "*": {
+                  fontSize: "inherit",
+                },
+              }}>
+              {crumb.icon}
+              {crumb.label}
+            </CustomLink>
+          );
+      })}
+    </Breadcrumbs>
+  );
+}
+
+export default RouteBasedBreadcrumbs;
--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -1,5 +1,7 @@
+import HomeIcon from "@mui/icons-material/Home";
 import React from "react";
 import { RouteObject } from "react-router-dom";
+import { RouteHandleObject } from "./models";
 
 const MainModule = React.lazy(() => import("@/modules/Main"));
 const HomePage = React.lazy(() => import("@/modules/Main/pages/HomePage"));
@@ -11,7 +13,28 @@ const mainRoutes: RouteObject[] = [
       {
         index: true,
         element: <HomePage />,
+        handle: {
+          crumb: () => ({
+            to: "/",
+            icon: <HomeIcon />,
+          }),
+          hideBreadcrumbs: true,
+        } as RouteHandleObject,
       },
+      // example of breadcrumb configuration for user detail page
+      // {
+      //   path: "users/:userId",
+      //   element: <UserDetailPage />,
+      //   loader: async (args) => ({
+      //     userId: args.params.userId,
+      //   }),
+      //   handle: {
+      //     crumb: (data) => ({
+      //       to: `users/${data.userId}`,
+      //       icon: <HomeIcon />,
+      //     }),
+      //   } as RouteHandleObject,
+      // },
     ],
   },
 ];
